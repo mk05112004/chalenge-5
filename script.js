ae3874b7189f529df6c86880b4f613a201d1b4e0
messaoud kawther <messaoudkawther2004@gmail.com>
2023-09-09 12:41:02 +0100

    this is my project

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const shortenBtn = document.getElementById("shortenBtn");
-    const originalUrlInput = document.getElementById("originalUrl");
-    const shortUrlSpan = document.getElementById("shortUrl");
+const urlForm = document.getElementById('urlForm');
+const originalURLInput = document.getElementById('originalURL');
+const shortcutURLDisplay = document.getElementById('shortcutURL');
 
-    shortenBtn.addEventListener("click", async () => {
-        const originalUrl = originalUrlInput.value;
+urlForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
 
-        try {
-            const response = await fetch("/api/shorten", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ originalUrl }),
-            });
+    const originalURL = originalURLInput.value;
 
-            if (response.ok) {
-                const data = await response.json();
-                shortUrlSpan.textContent = data.shortUrl;
-            } else {
-                console.error("Failed to shorten URL");
-            }
-        } catch (error) {
-            console.error(error);
+    try {
+        // Send the original URL to your backend to generate a shortcut URL
+        const response = await fetch('/api/generateShortcut', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ originalURL }),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            shortcutURLDisplay.innerHTML = `Shortcut URL: <a href="${data.shortcutURL}" target="_blank">${data.shortcutURL}</a>`;
+        } else {
+            // Handle errors here
+            console.error('Error generating shortcut URL');
         }
-    });
+    } catch (error) {
+        console.error('Error:', error);
+    }
 });
